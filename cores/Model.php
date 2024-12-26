524729976aebe711bb1d8e4f2a82ea6c386819d3
saldisucipto <saldisucipto@gmail.com>
2024-12-26 21:06:23 +0700

    Development : ~ On Progress Validation

--- cores/Model.php
+++ cores/Model.php
@@ -4,6 +4,13 @@ namespace app\cores;
 
 abstract class Model
 {
+    // Property untuk validate 
+    public const RULE_REQUIRED = 'required';
+    public const RULE_EMAIL = 'email';
+    public const RULE_MIN = 'min';
+    public const RULE_MAX = 'max';
+    public const RULE_MATCH = 'match';
+
     public function load_data($data)
     {
         foreach ($data as $key => $value) {
@@ -13,5 +20,14 @@ abstract class Model
         }
     }
 
-    public function validate() {}
+    // abstract function yang harus di implementasi di child classs 
+    /**
+     * Summary of rules
+     * @return array
+     */
+    abstract public function rules(): array;
+
+    public function validate()
+    {
+    }
 }
--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -16,4 +16,18 @@ class RegisterModel extends Model
     {
         echo "Create User!";
     }
+
+    // harus mengimplementasikan rules 
+    public function rules(): array
+    {
+        // logic validation ada disini 
+        return [
+            'firstname' => [self::RULE_REQUIRED],
+            'lastname' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8]],
+            'password_confirmation' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
+
+        ];
+    }
 }
